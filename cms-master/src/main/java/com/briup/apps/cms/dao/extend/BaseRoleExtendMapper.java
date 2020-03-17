e6e09add4f0bdf8674ce12ceb60905e65113796a
zengyyu <51160562+zengyyu@users.noreply.github.com>
2020-03-17 22:52:46 +0800

    Update BaseRoleExtendMapper.java

--- cms-master/src/main/java/com/briup/apps/cms/dao/extend/BaseRoleExtendMapper.java
+++ cms-master/src/main/java/com/briup/apps/cms/dao/extend/BaseRoleExtendMapper.java
@@ -5,12 +5,6 @@ import com.briup.apps.cms.bean.extend.BaseRoleExtend;
 
 import java.util.List;
 
-/**
- * @program: cms_jd1911
- * @description:
- * @author: charles
- * @create: 2019-11-15 15:13
- **/
 
 public interface BaseRoleExtendMapper {
     List<BaseRole> selectByUserId(long id);
