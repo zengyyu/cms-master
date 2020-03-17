198daedf94d579df63c30f7cd80b64641e9eff1f
zengyyu <51160562+zengyyu@users.noreply.github.com>
2020-03-17 22:59:23 +0800

    Update UserRoleVM.java

--- cms-master/src/main/java/com/briup/apps/cms/vm/UserRoleVM.java
+++ cms-master/src/main/java/com/briup/apps/cms/vm/UserRoleVM.java
@@ -3,10 +3,7 @@ package com.briup.apps.cms.vm;
 import java.util.List;
 
 /**
- * @program: cms_jd1911
- * @description: 用户角色
- * @author: charles
- * @create: 2019-11-17 21:21
+ *  用户角色
  **/
 
 public class UserRoleVM {
